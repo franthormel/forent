87fa32ea385de17bc4c31d54ef758ebec77c9b57
Francis Anthony <fcaboyo@gmail.com>
2023-11-03 20:45:21 +0800

    Add link button story

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { Metadata } from 'next';
 import LinkButton from '@/components/LinkButton';
 
-export default function Home() {
+export const metadata: Metadata = {
+  title: 'Homepage | Forent',
+  description: 'Homepage',
+}
 
+export default function Home() {
   return (
     <main className='flex flex-col space-y-4'>
       <h1 className='text-5xl font-bold'>Forent</h1>
---
+++ stories/LinkButton.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import LinkButton from "../components/LinkButton";
+
+const meta: Meta = {
+    title: "LinkButton",
+    component: LinkButton,
+    parameters: {
+        layout: "centered",
+    },
+    tags: ["autodocs"],
+} satisfies Meta<typeof LinkButton>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Example: Story = {
+    args: {
+        href: "",
+        text: "Button"
+    },
+};
