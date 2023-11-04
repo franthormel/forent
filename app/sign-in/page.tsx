b509ab08b3f996af0ebcd96b350e60aac698f86c
Francis Anthony <fcaboyo@gmail.com>
2023-11-04 13:44:11 +0800

    Update page.tsx

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,15 +1,27 @@
+import FormSubmitButton from "@/components/FormSubmitButton"
+import InputField from "@/components/InputField"
+
 export default function SignIn() {
     return (
-        <main>
-            <h3>Sign In</h3>
-            <form>
-                <p>Email</p>
-                <input type="email" />
-                <p>Password</p>
-                <input type="password" />
-                <br />
-                <input type="submit" value="Sign in" />
+        <main className="flex flex-col space-y-4">
+            <header className="text-4xl">Sign In</header>
+            <form method="POST" className="space-y-4">
+                <InputField
+                    htmlFor="email"
+                    label="Email"
+                    name="email"
+                    type="email"
+                    required={true}
+                />
+                <InputField
+                    htmlFor="password"
+                    label="Password"
+                    name="password"
+                    type="password"
+                    required={true}
+                />
+                <FormSubmitButton />
             </form>
         </main>
-    );
+    )
 }
