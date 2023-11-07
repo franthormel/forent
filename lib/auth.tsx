6cc3f0749ee8548e1d77318409e7339b336675f6
Francis Anthony <fcaboyo@gmail.com>
2023-11-07 20:29:41 +0800

    Formatting
    Remove unneeded components

--- lib/auth.tsx
+++ lib/auth.tsx
@@ -10,9 +10,9 @@ import { getServerSession } from "next-auth"
 import EmailProvider from "next-auth/providers/email"
 import GoogleProvider from "next-auth/providers/google"
 
+const prisma = new PrismaClient()
 // You'll need to import and pass this
 // to `NextAuth` in `app/api/auth/[...nextauth]/route.ts`
-const prisma = new PrismaClient()
 export const authOptions = {
   adapter: PrismaAdapter(prisma),
   debug: true,
