aaf06ba40fd565ae14c9a72f754aa5ee0e2bd365
Francis Anthony <fcaboyo@gmail.com>
2023-11-18 20:42:08 +0800

    Add UI button

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,16 +1,27 @@
-import { DM_Sans, DM_Serif_Display, Libre_Baskerville } from 'next/font/google'
+import { DM_Sans, DM_Serif_Display, Libre_Baskerville } from "next/font/google";
 
 export const textFont = DM_Sans({
-    subsets: ['latin'],
-    weight: "400",
-})
+  subsets: ["latin"],
+  weight: [
+    "100",
+    "200",
+    "300",
+    "400",
+    "500",
+    "600",
+    "700",
+    "800",
+    "900",
+    "1000",
+  ],
+});
 
 export const headerFont = DM_Serif_Display({
-    subsets: ['latin'],
-    weight: "400",
-})
+  subsets: ["latin"],
+  weight: "400",
+});
 
 export const logoFont = Libre_Baskerville({
-    subsets: ['latin'],
-    weight: "700"
-})
+  subsets: ["latin"],
+  weight: "700",
+});
---
+++ stories/Button.stories.ts
@@ -0,0 +1,42 @@
+import type { Meta, StoryObj, ArgTypes } from "@storybook/react";
+import Button, { ButtonProps } from "@/components/ui/Button";
+
+const meta: Meta = {
+  title: "Components/UI/Button",
+  component: Button,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof Button>;
+``;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const args: ButtonProps = {
+  text: "Button",
+  onClick: () => console.log("Clicked 🐭!"),
+};
+
+export const PrimaryColor: Story = {
+  args: args,
+};
+
+const secondaryArgs: ButtonProps = {
+  ...args,
+  color: "bg-orange-600",
+};
+
+export const SecondaryColor: Story = {
+  args: secondaryArgs,
+};
+
+const tertiaryArgs: ButtonProps = {
+  ...args,
+  color: "bg-cyan-300",
+};
+
+export const TertiaryColor: Story = {
+  args: tertiaryArgs,
+};
