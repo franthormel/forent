e96191b7fdc83b44eb490fc095836a63340c5d84
Francis Anthony <fcaboyo@gmail.com>
2023-11-22 20:50:41 +0800

    Update MapDisplay.tsx
    add geolocation, need fix

---
+++ components/map/MapDisplay.tsx
@@ -0,0 +1,105 @@
+"use client"
+
+import { Address } from '@/lib/listing';
+import { Feature, Map, View } from 'ol';
+import Geolocation from 'ol/Geolocation';
+import { ZoomSlider, defaults } from 'ol/control';
+import { Point } from 'ol/geom';
+import TileLayer from 'ol/layer/Tile';
+import VectorLayer from 'ol/layer/Vector';
+import { fromLonLat, toLonLat } from 'ol/proj';
+import OSM from 'ol/source/OSM';
+import VectorSource from 'ol/source/Vector';
+import { Circle as CircleStyle, Fill, Stroke, Style } from 'ol/style';
+import { useEffect, useRef, useState } from "react";
+
+// TODO: Add custom icon, optimize geolocation, and pan animation when map point is selected
+// https://openlayers.org/en/latest/examples/animation.html
+const mapZoomDefault = 2;
+
+// Map (Features)
+const positionFeature = new Feature();
+positionFeature.setStyle(
+    new Style({
+        image: new CircleStyle({
+            radius: 6,
+            fill: new Fill({
+                color: '#3399CC',
+            }),
+            stroke: new Stroke({
+                color: '#fff',
+                width: 2,
+            }),
+        }),
+    })
+);
+// Map (Layers)
+const mapLayers = [
+    new TileLayer({
+        source: new OSM()
+    }),
+    new VectorLayer({
+        source: new VectorSource({
+            features: [positionFeature],
+        }),
+    })
+];
+
+export default function MapDisplay() {
+    const zoom = useRef<number>(mapZoomDefault);
+    const [address, setAddress] = useState<Address>({ longitude: 0, latitude: 0 });
+
+    useEffect(() => {
+        // Map (View)
+        const mapView = new View({
+            center: fromLonLat([address.longitude!, address.latitude!]),
+            zoom: zoom.current,
+        });
+        // Map (Controls)
+        const mapControls = defaults({ attributionOptions: { collapsible: true } });
+        const mapControlZoomSlider = new ZoomSlider();
+        // Map
+        const map = new Map({
+            target: "map",
+            layers: mapLayers,
+            controls: mapControls,
+            view: mapView
+        });
+        map.addControl(mapControlZoomSlider);
+        map.on("singleclick", (e) => {
+            positionFeature.setGeometry(new Point(e.coordinate));
+
+            // Retain map zoom level between re-renders
+            zoom.current = map.getView().getZoom() ?? mapZoomDefault
+
+            // Change input field values
+            const lonLat = toLonLat(e.coordinate);
+            setAddress({
+                ...address,
+                longitude: lonLat[0],
+                latitude: lonLat[1]
+            })
+        });
+
+        // Geolocation
+        const geolocation = new Geolocation({
+            trackingOptions: {
+                enableHighAccuracy: true
+            },
+            // tracking: true,
+            projection: mapView.getProjection(),
+        });
+        geolocation.on('change:position', function () {
+            const coordinates = geolocation.getPosition();
+            positionFeature.setGeometry(coordinates ? new Point(coordinates) : undefined);
+        });
+
+        return () => map.dispose();
+    });
+
+    return <>
+        <div className='h-screen max-h-screen' id="map" tabIndex={1} />
+        <input type="hidden" name="addressLongitude" value={address.longitude} />
+        <input type="hidden" name="addressLatitude" value={address.latitude} />
+    </>;
+}
