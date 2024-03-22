b6cb0d35d340f7f08efa9b1e4885c18bf125db51
franthormel <fcaboyo@gmail.com>
2024-03-22 17:10:05 +0800

    Adjust search component
    refs #53

--- components/button-icons/index.tsx
+++ components/button-icons/index.tsx
@@ -7,7 +7,7 @@ export default function ButtonIcon({ children, props }: {
 }) {
     const dataCy = props.dataCy ?? "btn-icon";
 
-    return <div className="m-auto block cursor-pointer fill-gray-800"
+    return <div className="self-center block cursor-pointer fill-gray-800"
         onClick={props.onClick}
         data-cy={dataCy}
         aria-label={props.alt}>
--- components/search/index.tsx
+++ components/search/index.tsx
@@ -2,37 +2,31 @@
 
 import { useState } from 'react'
 import ButtonIconSearch from '../button-icons/search'
-import { searchAction, searchWidth } from "./functions"
-import { SearchWidth } from "./type"
+import { searchAction } from "./functions"
 
 interface SearchProps {
     dataCyInput?: string
     dataCyButtonIconSearch?: string
     placeholder?: string
-    /**
-     * Default is 'regular'
-     */
-    width?: SearchWidth
     onSubmit?: Function
 }
 
 export default function Search(props: SearchProps) {
     const action = () => searchAction(props.onSubmit)
     const [searchValue, setSearchValue] = useState('')
-    const width = searchWidth(props.width)
     const dataCyInput = props.dataCyInput ?? "search-input";
     const dataCyButtonIcon = props.dataCyButtonIconSearch ?? "search-btn-icon";
 
     // if drop shadow is removed, outline must be added
     return (
-        <div className="flex w-fit justify-between rounded-full bg-slate-50 px-6 py-0 shadow-md">
+        <div className="flex justify-between rounded-full bg-slate-50 px-6 py-0 shadow-md">
             <input type='search'
                 autoCapitalize='none'
                 data-cy={dataCyInput}
                 maxLength={128}
                 placeholder={props.placeholder}
                 spellCheck='false'
-                className={`bg-slate-50 focus:outline-none ${width} h-12`}
+                className="mr-2 h-14 w-full bg-slate-50 focus:outline-none"
                 onChange={(e) => setSearchValue(e.target.value)}
                 onKeyDown={(e) => {
                     const key = e.key.toLowerCase()
