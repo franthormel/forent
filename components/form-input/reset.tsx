80eb0ca2bdc1bdb58ae7fdbd63733ff8a882f8d7
Francis Anthony <fcaboyo@gmail.com>
2024-04-02 15:19:09 +0800

    Add form input reset component test
    refs #64

--- components/form-input/reset.tsx
+++ components/form-input/reset.tsx
@@ -1,4 +1,12 @@
-export default function FormInputReset() {
-    return <input type="reset" value="Reset"
-        className="cursor-pointer rounded-md border-2 border-transparent px-6 py-3 transition-all hover:border-slate-200" />;
+interface FormInputResetProps {
+    dataCy?: string
+}
+
+export default function FormInputReset(props: FormInputResetProps) {
+    return (
+        <input type="reset"
+            value="Reset"
+            className="cursor-pointer rounded-md border-2 border-transparent px-6 py-3 transition-all hover:border-slate-200"
+            data-cy={props.dataCy ?? "form-input-reset"} />
+    )
 }
