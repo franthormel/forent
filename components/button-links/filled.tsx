55e59eb882c5b5d1a84635974a5cc101f028a7a1
franthormel <fcaboyo@gmail.com>
2024-04-11 17:00:52 +0800

    Add button link components
    refs #77

---
+++ components/button-links/filled.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import ButtonFilled from "../buttons/filled";
+import { ButtonLinkProps } from "./types";
+
+export default function ButtonLinkFilled(props: ButtonLinkProps) {
+    return (
+        <Link href={props.href} data-cy={props.dataCy ?? "button-link-filled"}>
+            <ButtonFilled text={props.text} />
+        </Link>
+    )
+}
