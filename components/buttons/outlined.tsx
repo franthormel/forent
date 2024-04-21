12380d05c2bb50036ef3d6d50f56dc1de4bb5f01
franthormel <fcaboyo@gmail.com>
2024-04-21 17:14:31 +0800

    Add effect outlined button is hovered
    refs #50

--- components/buttons/outlined.tsx
+++ components/buttons/outlined.tsx
@@ -3,7 +3,7 @@ import { ButtonProps } from "./types";
 // NOTE: Button styles must be consistent (must apply the same applicable CSS to all button components)
 export default function ButtonOutlined(props: ButtonProps) {
     return (
-        <button className="rounded-full border-2 border-gray-800 px-10 py-4 transition-all hover:shadow-md"
+        <button className="rounded-full border-2 border-gray-800 px-10 py-4 text-gray-800 transition-all hover:bg-gray-800 hover:text-slate-50"
             data-cy={props.dataCyBtn ?? "button"}
             onClick={props.onClick}>
             {props.text ?? "Button"}
