771498174eff8499a2aea209ad8dbc571353a804
franthormel <fcaboyo@gmail.com>
2024-04-24 21:07:58 +0800

    Fix card listing layout
    refs #50

--- components/card-listing/index.tsx
+++ components/card-listing/index.tsx
@@ -35,35 +35,36 @@ export default function CardListing(props: CardListingProps) {
     const dataCyAddressLine2 = props.dataCyAddressLine2 ?? "card-listing-address-line2"
 
     return (
-        <Link className="w-80 cursor-pointer rounded-md bg-slate-50 p-0 shadow-md transition-all hover:shadow-xl"
-            href={`/listing/${props.id}`}>
-            <Image
-                className="h-3/5 w-full rounded-t-md object-cover"
-                alt="Search"
-                src={props.imgUrl}
-                height={300}
-                width={300}
-                data-cy={dataCyImage}
-            />
-            <div className="grid grid-flow-row auto-rows-max gap-2 p-5">
-                <header className="text-xl font-bold" data-cy={dataCyPrice}>
-                    {props.price}
-                </header>
-                <p className="text-base">
-                    <span data-cy={dataCyBeds}>
-                        <span className="font-bold">{beds}</span> bed{beds > 1 && "s"}
-                    </span>
-                    <span className="ml-3" data-cy={dataCyBaths}>
-                        <span className="font-bold">{baths}</span> bath{baths > 1 && "s"}
-                    </span>
-                    <span className="ml-3" data-cy={dataCyArea}>
-                        <span className="font-bold">{props.area}</span> sqm
-                    </span>
-                </p>
-                <p className="text-xs">
-                    <p data-cy={dataCyAddressLine1}>{props.addressLine1}</p>
-                    <p data-cy={dataCyAddressLine2}>{props.addressLine2}</p>
-                </p>
+        <Link href={`/listing/${props.id}`}>
+            <div className="w-80 cursor-pointer rounded-md bg-slate-50 p-0 shadow-md transition-all hover:shadow-xl">
+                <Image
+                    className="h-3/5 w-full rounded-t-md object-cover"
+                    alt="Search"
+                    src={props.imgUrl}
+                    height={300}
+                    width={300}
+                    data-cy={dataCyImage}
+                />
+                <div className="grid grid-flow-row auto-rows-max gap-2 p-5">
+                    <header className="text-xl font-bold" data-cy={dataCyPrice}>
+                        {props.price}
+                    </header>
+                    <p className="text-base">
+                        <span data-cy={dataCyBeds}>
+                            <span className="font-bold">{beds}</span> bed{beds > 1 && "s"}
+                        </span>
+                        <span className="ml-3" data-cy={dataCyBaths}>
+                            <span className="font-bold">{baths}</span> bath{baths > 1 && "s"}
+                        </span>
+                        <span className="ml-3" data-cy={dataCyArea}>
+                            <span className="font-bold">{props.area}</span> sqm
+                        </span>
+                    </p>
+                    <p className="text-xs">
+                        <p data-cy={dataCyAddressLine1}>{props.addressLine1}</p>
+                        <p data-cy={dataCyAddressLine2}>{props.addressLine2}</p>
+                    </p>
+                </div>
             </div>
         </Link>
     )
