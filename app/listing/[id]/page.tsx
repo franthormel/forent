ee2b5bd7bc6b11161026eec9d7f6a423a85426f7
franthormel <fcaboyo@gmail.com>
2024-05-22 16:50:32 +0800

    Create listing component
    refs #100

--- app/listing/[id]/page.tsx
+++ app/listing/[id]/page.tsx
@@ -1,9 +1,4 @@
-import ListingBanner from "@/components/_app/listing/banner";
-import ListingContact from "@/components/_app/listing/contact";
-import ListingDescription from "@/components/_app/listing/description";
-import ListingMainInfo from "@/components/_app/listing/main-info";
-import ListingMap from "@/components/_app/listing/map";
-import PageLayout from "@/components/_app/page-layout";
+import Listing from "@/components/_app/listing";
 import prisma from "@/lib/db";
 
 export default async function ListingPage({ params }: { params: { id: string } }) {
@@ -17,22 +12,26 @@ export default async function ListingPage({ params }: { params: { id: string } }
             user: true,
         }
     })
-    const currentPrice = listing.prices.filter((p) => p.isCurrent).at(0)!.value;
+    const currentPrice = listing.prices.filter((p) => p.isCurrent).at(0)!.value.toNumber();
 
     return (
-        // TODO: Convert this into another component so it can be reused in the Preview page
-        <PageLayout>
-            {/* Photos & main information */}
-            <div className="grid auto-rows-auto gap-y-11">
-                <ListingBanner imageUrls={listing.imageUrls} listingId={listing.id} />
-                <ListingMainInfo price={currentPrice.toNumber()} beds={listing.beds} baths={listing.baths}
-                    area={listing.area.toNumber()} availableDate={listing.availableDate}
-                    addressLine={listing.address!.addressLine} city={listing.address!.city}
-                    state={listing.address!.state} zipCode={listing.address!.zipcode} />
-            </div>
-            <ListingContact name={listing.user.name} contactNumber={listing.user.contactNumber} email={listing.user.email} />
-            <ListingDescription description={listing.description} />
-            <ListingMap lat={listing.address!.latitude.toNumber()} lon={listing.address!.longitude.toNumber()} />
-        </PageLayout>
+        <Listing id={listing.id}
+            imageUrls={listing.imageUrls}
+            price={currentPrice}
+            beds={listing.beds}
+            baths={listing.baths}
+            area={listing.area.toNumber()}
+            availableDate={listing.availableDate}
+            addressLine={listing.address!.addressLine}
+            city={listing.address!.city}
+            state={listing.address!.state}
+            zipCode={listing.address!.zipcode}
+            name={listing.user.name}
+            contactNumber={listing.user.contactNumber}
+            email={listing.user.email}
+            description={listing.description}
+            latitude={listing.address!.latitude.toNumber()}
+            longitude={listing.address!.longitude.toNumber()}
+        />
     )
 }
