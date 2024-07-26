7da8e384912093f5abde0734fdcd560ae66c06ff
Francis Anthony <fcaboyo@gmail.com>
2024-07-26 11:52:12 +0800

    Add pagination component
    refs #49

---
+++ components/pagination/index.tsx
@@ -0,0 +1,49 @@
+import PaginationItemNav from "./item-nav"
+import PaginationItemNumber from "./item-number"
+
+export interface PaginationProps {
+    pages: number
+    /**
+     * Current page defaults to `1` if no value is provided.
+     */
+    currentPage?: number
+}
+
+// TODO: Style when many pages (need to break it somewhere, must work for all screen screens)
+export default function Pagination(props: PaginationProps) {
+    const pageNumbers = Array.from({ length: props.pages }, (elem, i) => i + 1)
+    // Show the previous button only if the current page is not the first page
+    const showPreviousButton = props.currentPage && props.currentPage >= 1
+    // Show the next button only if the current page is not the last page
+    const showNextButton = props.currentPage === undefined || props.currentPage < props.pages
+
+    return (
+        <div className="flex gap-3">
+            {/* Previous (hidden if the current page is the first page) */}
+            {showPreviousButton &&
+                <PaginationItemNav>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" width="24px" viewBox="0 -960 960 960">
+                        <path d="M560-240 320-480l240-240 56 56-184 184 184 184-56 56Z" />
+                    </svg>
+                </PaginationItemNav>
+            }
+            {pageNumbers.map((pageNumber) => {
+                const isCurrentPage = props.currentPage === pageNumber
+                console.log(isCurrentPage, props.currentPage, pageNumber)
+                return <PaginationItemNumber
+                    page={pageNumber}
+                    key={pageNumber}
+                    isCurrentPage={isCurrentPage}
+                />
+            })}
+            {/* Next (hidden if current page is the last page) */}
+            {showNextButton &&
+                <PaginationItemNav>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" width="24px" viewBox="0 -960 960 960">
+                        <path d="M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z" />
+                    </svg>
+                </PaginationItemNav>
+            }
+        </div>
+    )
+}
---
+++ stories/pagination/Pagination.stories.tsx
@@ -0,0 +1,50 @@
+import Pagination from "@/components/pagination";
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta: Meta<typeof Pagination> = {
+    title: "Pagination",
+    component: Pagination,
+    tags: ["autodocs"],
+} satisfies Meta<typeof Pagination>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {
+        pages: 5
+    }
+};
+
+export const FirstPage: Story = {
+    args: {
+        pages: 3,
+    }
+};
+
+export const MiddlePage: Story = {
+    args: {
+        pages: 3,
+        currentPage: 2,
+    }
+};
+
+export const LastPage: Story = {
+    args: {
+        pages: 3,
+        currentPage: 3,
+    }
+};
+
+export const SinglePage: Story = {
+    args: {
+        pages: 1,
+    }
+};
+
+export const ManyPages: Story = {
+    args: {
+        pages: 101,
+    }
+};
+
