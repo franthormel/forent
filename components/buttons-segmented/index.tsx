a98c7081bbf8d493f896f713e078600ab2e4c7be
Francis Anthony <fcaboyo@gmail.com>
2024-07-30 09:23:53 +0800

    Add segmented button component
    refs #49

---
+++ components/buttons-segmented/index.tsx
@@ -0,0 +1,18 @@
+export interface ButtonsSegmentedProps {
+    values: string[]
+}
+
+// TODO: Correct hover style on currently active segment
+export default function ButtonsSegmented(props: ButtonsSegmentedProps) {
+    return (
+        <div className="flex w-full">
+            {props.values.map((value, index, arrayValue) =>
+                <div key={`button-segmented-${index}`}
+                    className="w-12 cursor-pointer border-y-[1px] border-l-[1px] border-gray-400 py-2 text-center 
+                            first:bg-amber-400 last:border-r-[1px] hover:bg-amber-200"
+                    onClick={(e) => { }}>
+                    {value}
+                </div>)}
+        </div>
+    );
+}
---
+++ stories/buttons-segmented/ButtonsSegmented.stories.tsx
@@ -0,0 +1,17 @@
+import ButtonsSegmented from "@/components/buttons-segmented";
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta: Meta<typeof ButtonsSegmented> = {
+    title: "Buttons/Segmented",
+    component: ButtonsSegmented,
+    tags: ["autodocs"],
+} satisfies Meta<typeof ButtonsSegmented>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Example: Story = {
+    args: {
+        values: ["Any", "1", "2", "3", "4", "5+"]
+    }
+};
