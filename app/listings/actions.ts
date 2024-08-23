5cfd24c2450c630ba11c0ffa841819d6828a37ad
Francis Anthony <fcaboyo@gmail.com>
2024-08-23 14:33:48 +0800

    Add filter for beds/baths
    refs #49

--- app/listings/actions.ts
+++ app/listings/actions.ts
@@ -2,7 +2,12 @@
 
 import prisma from "@/lib/db";
 import { DEFAULT_LIST_FILTERS } from "./constants";
-import { Listing, ListingsSearchFilters, PrismaListing } from "./types";
+import {
+  BedsBathsOption,
+  Listing,
+  ListingsSearchFilters,
+  PrismaListing,
+} from "./types";
 
 const prismaListingMapper = (dbListing: PrismaListing) => {
   const dbListingPrice = dbListing.prices
@@ -34,8 +39,6 @@ const prismaListingMapper = (dbListing: PrismaListing) => {
 export async function fetchMatchedListings(
   filters: ListingsSearchFilters = DEFAULT_LIST_FILTERS
 ): Promise<Listing[]> {
-  // TODO: Use beds filter values
-  // TODO: Use baths filter values
   const prismaListings = await prisma.listing.findMany({
     include: {
       address: true,
@@ -73,5 +76,43 @@ export async function fetchMatchedListings(
       },
     },
   });
+
+  // It is easier to filter the beds/baths values programatically rather than using Prisma's filtering API for the use case.
+  return prismaListings
+    .filter((listing) =>
+      bedsBathsOptionFilter(filters.beds.value, listing.beds)
+    )
+    .filter((listing) =>
+      bedsBathsOptionFilter(filters.baths.value, listing.baths)
+    )
+    .map(prismaListingMapper);
   return prismaListings.map(prismaListingMapper);
+}
+
+function bedsBathsOptionFilter(
+  option: BedsBathsOption,
+  value: number
+): boolean {
+  switch (option) {
+    case BedsBathsOption.ANY:
+      return true;
+      break;
+    case BedsBathsOption.ONE:
+      return value === 1;
+      break;
+    case BedsBathsOption.TWO:
+      return value === 2;
+      break;
+    case BedsBathsOption.THREE:
+      return value === 3;
+      break;
+    case BedsBathsOption.FOUR:
+      return value === 4;
+      break;
+    case BedsBathsOption.MORE_THAN_FIVE:
+      return value >= 5;
+      break;
+    default:
+      return true;
+  }
 }
